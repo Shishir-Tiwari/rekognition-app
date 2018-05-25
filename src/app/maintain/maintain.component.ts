dda453cc57d3c7515099ee2d87de7d0d64b67288
Grace <grace.d'mello@btfinancilagroup.com>
2018-05-25 12:26:58 +1000

    try

--- src/app/maintain/maintain.component.ts
+++ src/app/maintain/maintain.component.ts
@@ -11,21 +11,10 @@ export class MaintainComponent implements OnInit {
   messgae: string;
   facesLength: any;
   hideImage: boolean = true;
-  camera: any
-  FILE_URI: any
   constructor(private appService: AppService) { }
 
   ngOnInit() {
-
-    document.addEventListener("deviceready", function() {
-    //  this.camera = navigator.camera;
-    //  this.FILE_URI = Camera.DestinationType.FILE_URI
-    alert('after'); 
-    
-    }, false); 
-    
-    }
-    
+  }
 
   deleteAllFaces() {
     this.appService.getAllFaces()
@@ -54,10 +43,10 @@ export class MaintainComponent implements OnInit {
   }
 
   captureImage() {
-    if ( this.camera) {
-      this.camera.getPicture(this.onSuccess, this.onFail, {
+    if (navigator.camera) {
+      navigator.camera.getPicture(this.onSuccess, this.onFail, {
         quality: 25,
-        destinationType: this.FILE_URI,
+        destinationType: Camera.DestinationType.FILE_URI,
         saveToPhotoAlbum: true
       });
     }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+document.addEventListener("deviceready", function() {
+  platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+}, false);
+
